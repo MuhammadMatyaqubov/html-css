1a29211f4dff809934ab35a5c7ea9ae4b421ee0b
MuhammadMatyaqubov <matyaqubovashaxista@gmail.com>
2026-04-08 10:57:25 +0500

    Add files via upload

---
+++ lesson_11/code.js
@@ -0,0 +1,13 @@
+// let UserName = "Ali"; // String
+// const age = 24; // Number
+// const UserAdmin = false;
+// console.log(UserName , age, UserAdmin)
+// const message = prompt("Parol")
+// const rozlik = confirm("Aniqmi")
+// console.log(message , rozlik)
+const number = prompt("Son ")
+for(let i = 1; 1 <=10; i++){
+    console.log(
+        ${i} /
+    )
+}
